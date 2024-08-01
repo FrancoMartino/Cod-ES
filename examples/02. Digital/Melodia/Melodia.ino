f6bcb7a79cd2f7e349dd16476f0cd25137c2a3cb
FrancoMartino <95370336+FrancoMartino@users.noreply.github.com>
2024-08-01 16:20:35 -0300

    Update Melodia.ino

--- examples/02. Digital/Melodia/Melodia.ino
+++ examples/02. Digital/Melodia/Melodia.ino
@@ -21,7 +21,7 @@
 #include "notas.h" // Incluir el archivo de encabezado que contiene las definiciones de las notas musicales.
 
 entero melodia[] = {
-  NOTA_DO4, NOTA_SOL3, NOTA_SOL3, NOTA_LA3, NOTA_SOL3, 0, NOTA_SI3, NOTA_C4
+  NOTA_DO4, NOTA_SOL3, NOTA_SOL3, NOTA_LA3, NOTA_SOL3, 0, NOTA_SI3, NOTA_DO4
 };
 
 entero duraciones[] = {
@@ -29,13 +29,13 @@ entero duraciones[] = {
 };
 
 inicio
-  repetir (entero nota = 0; nota < 8; nota++) {
+  repetir (entero nota = 0, nota < 8, nota++)
     entero duracion = 1000 / duraciones[nota]; // Calcular la duración de la nota en milisegundos.
     
     tono(8, melodia[nota], duracion); // Generar un tono con la nota actual y su duración.
     esperar(duracion * 1.30, MILISEGUNDOS); // Esperar un tiempo ligeramente mayor que la duración de la nota.
     apagarTono(8); // Apagar el tono después de la duración especificada.
-  }
+  cerrar
 fin
 
 bucle
